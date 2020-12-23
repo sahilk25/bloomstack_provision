fe65100903deeb438fd999c55fac3eb2e1c7decb
Sahil <sahil.kamboj25@gmail.com>
2020-12-23 12:51:19 +0530

    feat: install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+apt install python3-pip -y
+python3 -m easy_install --upgrade pyOpenSSL
+pip3 install ansible
+pip3 install redis
+lone https://github.com/sahilk25/bloomstack_provision.git
+cd bloomstack_provision
+ansible-playbook bench.yaml -vv
